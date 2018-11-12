ff96cfb569e583736f072d015e16cb41ebbcded3
Nat Henry <nathenry@stash.ihme.washington.edu>
2018-11-12 15:58:56 -0800

    Updating geocoding execution to work with new API query functions, including new command-line arguments.

--- geocode/batch_geocode.py
+++ geocode/batch_geocode.py
@@ -2,41 +2,67 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Mar 17 10:37:07 2017
-@author: nathenry
+@author: Nathaniel Henry, nathenry@uw.edu
 
-This module includes a series of functions that allow for automated geocoding
-using the Google Maps, OpenStreetMaps, and GeoNames APIs.
-
-You can edit the paths within this program, or else call it from the command line.
+This file defines the command line interface for the automated geocoding 
+application, which allows the user to geocode many locations in succession using
+the the Google Maps, OpenStreetMaps, GeoNames, and/or FuzzyG APIs. For more
+information about how to use this tool, please see the repository README.
 
 Written in Python 3.6
 """
 
 import argparse
-import json
 import numpy as np
 import pandas as pd
-import sys
-import geocode.query_funcs as qf
-from os.path import join
-from numpy.random import choice as randoneof
-from time import sleep
+from encodings.aliases import aliases
+from geocode import query_funcs as qf
 
 
-def check_cmd(cmd_arg,default_arg,value_name):
-    """This function determines whether to use the coomand-line value or the 
-    default value for a given program parameter."""
-    if cmd_arg:
-        print("The command-line argument, '{}', will be used for the {}.".format(cmd_arg,
-                                                                               value_name))
-        sleep(1)
-        return cmd_arg
+def read_to_pandas(fp):
+    """Read an input Excel or CSV file as a pandas DataFrame, testing a variety
+    of encodings."""
+    readfun = pd.read_csv if fp.lower().endswith('.csv') else pd.read_excel
+    test_encodings = ['utf-8','latin1'] + list(aliases.keys())
+    valid_encoding = None
+    for encoding in test_encodings:
+        try:
+            df = readfun(fp, encoding=encoding)
+            valid_encoding = encoding
+            break
+        except UnicodeDecodeError:
+            pass
+    if valid_encoding is None:
+        raise UnicodeDecodeError(encoding='All standard encodings', reason='', 
+                                 object=f'file {fp}', start=0, end=0)
+    return (df, encoding)
+
+
+def write_pandas(df, fp, encoding):
+    """Write a pandas DataFrame to a CSV or Excel file using a known file
+    encoding."""
+    if fp.lower().endswith('.csv'):
+        df.to_csv(fp, encoding=encoding, index=False)
     else:
-        print("The program's default value, '{}', will be used for the {}.".format(default_arg,
-                                                                                   value_name))
-        sleep(1)
-        return default_arg
+        df.to_excel(fp, encoding=encoding, index=False)
+    return None
 
+
+
+def rearrange_fields(gc_df):
+    """Rearrange the column order of a geocoded dataframe and drop unnecessary 
+    fields."""
+    cols = list(gc_df.columns)
+    prefixes = sorted(
+        list(set( [c[0:c.index('_')] for c in cols] )),
+        key=lambda s: s.lower()
+    )
+    if 'best' not in prefixes:
+        prefixes = ['best'] + prefixes
+    # Keep only the following fields from the results of each geocoding tool
+    suffixes = ['name','type','long','lat','buffer']
+    all_cols = [f'{p}_{s}' for p in prefixes for s in suffixes]
+    return gc_df.reindex(labels=all_cols, axis='columns')
 
 
 
@@ -45,103 +71,53 @@ if __name__ == "__main__":
     This section of the program will run from the command line or an 
     interactive prompt.
     """
-    # System setup for users at IHME
-    if 'win' in sys.platform:
-        j_head = "J:/"
-        h_head = "H:/"
-    else:
-        j_head = "/home/j/"
-        # If you are adding paths inline, change this to your own username
-        h_head = "homes/nathenry"
-
-    ######################################################################
-    # SET DEFAULT FILEPATHS AND VALUES
-    ######################################################################
-
-    # Set the default input and output files
-    # These files will be used if no command line arguments are passed
-    in_file = join(j_head,'temp/nathenry/testing_google_maps_query/test_query_sheet.xlsx')
-    out_file = join(j_head,'temp/nathenry/testing_google_maps_query/test_query_output.xlsx')
-    
-    # Set the default name of the column to be geocoded
-    geocode_col="for_geocoding"
-    # Set the default name of the column containing ISO-2 codes
-    # (None is an acceptable value)
-    iso2_col=None
-
-    # Data key for the Google Maps Geocoding API
-    # Generate your own here:
-    # https://developers.google.com/maps/documentation/geocoding/get-api-key
-    gmaps_key = None
-    # Data key for the Google Maps Static Maps API
-    # As of September 2017, this is different from the Google Maps Geocoding API
-    # Generate your own here: 
-    # https://developers.google.com/maps/documentation/static-maps/
-    static_maps_key = None
-    # Geonames username
-    # Create your own account and activate your username at http://geonames.org
-    geonames_username = None
-    # If no default key are set or passed through the command line, default API
-    #   keys and usernames will be read from the following file.
-    keys_file = join(j_head,'Project/Mortality/shocks/gbd_2017/process_inputs/keys.json')
-
     ######################################################################
     # SET UP AND READ COMMAND LINE ARGUMENTS, IF ANY
     ######################################################################
+
     print("\n*********************************")
-    print("***     SET UP PARAMETERS     ***")
+    print("***      READ PARAMETERS      ***")
     print("*********************************")
 
-    # If any command line arguments were passed, set up and read args
-    if len(sys.argv) > 1:
-        # This means some arguments were passed
-        # Set up possible arguments
-        parser = argparse.ArgumentParser()
-        parser.add_argument("-i","--infile",type=str,help="The location of the input Excel file")
-        parser.add_argument("-o","--outfile",type=str,help="The name of the output file")
-        parser.add_argument("-a","--address",type=str,help="The name of the column to geocode")
-        parser.add_argument("-s","--iso",type=str,help="The name of the file's ISO2 column (if any)")
-        parser.add_argument("-k","--keygm",type=str,help="Your own activated Google Maps geocoding key (optional)")
-        parser.add_argument("-m","--keystatic",type=str,help="Your own activated Google Maps Static Maps key (optional)")
-        parser.add_argument("-g","--geonames",type=str,help="Your own activated Geonames username (optional)")
-        c_args = parser.parse_args()
+    # Set up command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--infile", type=str,
+                        help="The full filepath of the input Excel or CSV file")
+    parser.add_argument("-o", "--outfile", type=str, 
+                        help="The full filepath of the output Excel or CSV file")
+    parser.add_argument("-a", "--address", type=str, default="for_geocoding",
+                        help="The name of the address field to geocode")
+    parser.add_argument("-s", "--iso", type=str,
+                        help="The name of the file's ISO2 field (if any)")
+    parser.add_argument("-k", "--keygm", type=str, 
+                        help="Activated Google Maps geocoding key")
+    parser.add_argument("-g", "--geonames", type=str, 
+                        help="Activated Geonames username")
+    parser.add_argument(
+        "-u", "--usetools", type=str, default='GM,OSM,GN,FG', 
+        help="""Comma-separated string listing geocoding web tools to query. 
+             Valid items in this list include GM (Google Maps), OSM 
+             (OpenStreetMap), GN (GeoNames), and FG (FuzzyG). Leaving this 
+             argument blank will query all available tools as a default 
+             UNLESS the Google Maps key and/or GeoNames username are blank. 
+             Example valid arguments: '-u GN'; '-u GN,GM,FG'; '-u OSM,GM,FG'
+             """
+    )
+    parser.add_argument(
+        "-r", "--resultspersource", type=int, default=2,
+        help="How many results should be returned from each web geocoding tool?"
+    )
+    parser.add_argument(
+        "-b", "--buffer", type=float, default=15,
+        help="""The maximum acceptable 'buffer size' (bounding box diagonal 
+             distance) that a valid geocoded location can have, in kilometers. 
+             The default is 15 km.
+             """
+    )
+    c_args = parser.parse_args()
 
-        # Check the command line values:
-        in_file = check_cmd(c_args.infile,in_file,"input spreadsheet filepath")
-        out_file = check_cmd(c_args.outfile,out_file,"output spreadsheet filepath")
-        geocode_col = check_cmd(c_args.address,geocode_col,"column name to geocode")
-        iso2_col = check_cmd(c_args.iso,iso2_col,"ISO2 column (if any)")
-        # Quietly add the Google Maps and Geonames keys
-        gmaps_key = c_args.keygm or gmaps_key
-        static_maps_key = c_args.keystatic or static_maps_key
-        geonames_username = c_args.geonames or geonames_username
-        # Determine whether or not maps will be made
-        make_maps = not(c_args.nomaps)
-        if not(make_maps):
-            print("No vetting maps will be made for this geocoding run.")
-    else:
-        print("No command line arguments were passed - all default values will be used.")
-
-    # Check if any program-specific Google Maps keys or Geonames usernames
-    #  have been passed. If not, read some default values from a file.
-    if np.any([i is None for i in [gmaps_key, static_maps_key, geonames_username]]):
-        # Load the file containing API keys
-        ## LOAD THE STATIC MAPS KEY
-        with open(keys_file) as f:
-            keys_dict = json.load(f)
-        if gmaps_key is None:
-            print("No Google Maps Geocoding API key has been provided;")
-            print("A default API key will be read from file.")
-            gmaps_key = str(randoneof(keys_dict['google_geocoding']))
-        if static_maps_key is None:
-            print("No Google Maps Static Maps API key has been provided;")
-            print("A default API key will be read from file.")
-            static_maps_key = str(randoneof(keys_dict['google_static_maps']))
-        if geonames_username is None:
-            print("No Geonames username has been provided;")
-            print("A default API key will be read from file.")
-            geonames_username = str(randoneof(keys_dict['geonames']))
-
+    # Parse the web tools to execute
+    execute_apps = [i.upper() for i in c_args.usetools.split(',')]
 
     ######################################################################
     # CODE EXECUTES
@@ -150,28 +126,23 @@ if __name__ == "__main__":
     print("***      BEGIN GEOCODING      ***")
     print("*********************************")
     print("Reading input file...")
-    if in_file.lower().endswith('.csv'):
-        df = pd.read_csv(in_file, encoding='latin1')
-    else:
-        df = pd.read_excel(in_file)
-    print("Geocoding using Google Maps...")
-    expanded = qf.gm_geocode_data_frame(df,
-                                     api_key=gmaps_key,
-                                     address_col=geocode_col,
-                                     iso_2_col=iso2_col)
-    print("Geocoding using OpenStreetMap...")
-    expanded = qf.osm_geocode_data_frame(expanded,
-                                      address_col=geocode_col)
-    print("Geocoding using GeoNames...")
-    expanded = qf.geonames_geocode_data_frame(expanded,
-                                           address_col=geocode_col,
-                                           iso_2_col=iso2_col,
-                                           username=geonames_username)
+    df, encoding = read_to_pandas(c_args.infile)
 
-    print("Exporting output to Excel...")
-    if out_file.lower().endswith('.csv'):
-        expanded.to_csv(out_file, encoding='latin1', index=False)
-    else:
-        expanded.to_excel(out_file, index=False)
-    print("Your output file is now ready to view at {} !".format(out_file))
+    print(f"Geocoding {df.shape[0]} rows of data...")
+    geocoded_cols = df.apply(
+        lambda row: geocode_row(
+            address=row[c_args.address], iso=row[c_args.iso],
+            gm_key=c_args.keygm, gn_key=c_args.geonames,
+            execute_names=execute_apps, results_per_app=c_args.resultspersource,
+            max_buffer=c_args.buffer
+        ),
+        axis=1
+    )
+    geocoded_cols = rearrange_fields(geocoded_cols)
+    df_with_geocoding = pd.concat([df, geocoded_cols], axis=1)
+
+    print("Exporting output to file...")
+    write_pandas(df=df_with_geocoding, fp=c_args.outfile, encoding=encoding)
+
+    print(f"Your output file is now ready to view at {c_args.outfile} !")
     print("\nGEOCODING COMPLETE")
