71ab0bac0b518db4ba2ce2e74700b5ed4aade026
Nat Henry <nathenry@stash.ihme.washington.edu>
2018-12-05 15:40:57 -0800

    Updating import statement for Python 3.

--- geocode/batch_geocode.py
+++ geocode/batch_geocode.py
@@ -16,7 +16,7 @@ import argparse
 import numpy as np
 import pandas as pd
 from encodings.aliases import aliases
-import geocode.query_funcs
+import query_funcs
 
 
 def read_to_pandas(fp, encoding='detect'):
