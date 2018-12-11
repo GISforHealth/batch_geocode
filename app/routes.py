b3d2e93f5d37ed990dfa79d60dad06aa6ce0a26c
Nat Henry <nathenry@stash.ihme.washington.edu>
2018-12-10 17:04:06 -0800

    Updating index page to geocode with new fields.

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,40 @@
-from flask import render_template, request, flash
+from flask import render_template, request, flash, Response
 from app import app
 from app.forms import SubmitForm
 from geocode import batch_geocode
+import time
 
 @app.route('/')
 @app.route('/index', methods=['GET','POST'])
 def index():
-	form = SubmitForm()
-	if request.method == 'POST':
-		if form.validate_on_submit():
-			infile = request.form['infile']
-			outfile = request.form['outfile']
-			address = request.form['address']
-			iso = request.form['iso']
-			key = request.form['key']
-			geonames = request.form['geonames']
-			usetools = request.form['usetools']
-			encoding = request.form['encoding']
-			resultsper = request.form['resultsper']
-			buffer = request.form['buffer']
-			batch_geocode.geocode_from_flask(infile=infile, iso=iso, outfile=outfile, keygm=key, geonames=geonames,
-											 address=address, usetools=usetools, encoding=encoding,
-											 resultspersource=resultsper, buffer=buffer)
-			flash('Your output file for input file, {}, usingtools {}, with {} results per source, and a buffer of {} '
-				  'is now ready to view at {}'.format(form.infile.data, form.usetools.data, form.resultsper.data,
-														form.buffer.data, form.outfile.data))
-			return render_template('index.html', title='Home', form=form)
-		else:
-			flash('Need to enter all required fields')
-	return render_template('index.html', title='Home', form=form)
+    form = SubmitForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            # Define the string of tools to use
+            usetools = list()
+            if form.use_gm.data: usetools.append('GM')
+            if form.use_osm.data: usetools.append('OSM')
+            if form.use_gn.data: usetools.append('GN')
+            if form.use_fg.data: usetools.append('FG')
 
+            batch_geocode.geocode_from_flask(
+                infile=form.infile.data,
+                outfile=form.outfile.data, 
+                encoding=form.encoding.data, 
+                address=form.address.data,
+                iso=form.iso.data,
+                keygm=form.key.data, 
+                geonames=form.geonames.data,
+                usetools=usetools, 
+                resultspersource=form.resultsper.data, 
+                geo_buffer=form.geo_buffer.data
+            )
+            flash(f"""Your output file for input file, {form.infile.data}, using 
+                      tools {usetools}, with {form.resultsper.data} 
+                      results per source, and a buffer of {form.geo_buffer.data} is 
+                      now ready to view at {form.outfile.data}.""")
+            return render_template('index.html', title='Home', form=form)
+        else:
+            flash('Need to enter all required fields')
+    return render_template('index.html', title='Home', form=form)
+    
