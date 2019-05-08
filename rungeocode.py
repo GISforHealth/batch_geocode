31c162e7c213cd5c02d2fba122e1277978a55669
mlc314 <mlc314@uw.edu>
2019-05-08 14:45:43 -0700

    bigger success message, fair cluster path

--- rungeocode.py
+++ rungeocode.py
@@ -5,6 +5,6 @@ HOST = socket.gethostname()
 PORT = 2301
 
 if __name__ == "__main__":
-    print( f"*** Now running application on http://{HOST}:{PORT} ***" )
+    print( f"*** Now running application on http://{HOST}.cluster.ihme.washington.edu:{PORT} ***" )
     app.run(debug=True, host=HOST, port=PORT)
     
